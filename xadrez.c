20d5399fd26e5770bf8724d9b376f3f424ec66f8
clararuperti <anaclararuperti@gmail.com>
2025-03-05 14:50:10 +0000

    Adicionei a movimentação da torre, dama e bispo.

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,28 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int torre=0, bispo=0, dama;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    for (dama= 0; dama<=8; dama++){ 
+      printf("Esquerda.\n"); 
+    }
+    
+    printf("A dama se moveu oito casas para a esquerda! \n");
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    while (torre<=5){ 
+        printf("Direita.\n");
+        torre++;
+    }
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("A torre se moveu cinco casas para a direita! \n");
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    do
+    {
+      printf("Cima e Direita.\n");
+      bispo++;  
+    } while (bispo<=5);
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    printf("O bispo se moveu cinco casas para cima e direita! \n");
 
     return 0;
 }
